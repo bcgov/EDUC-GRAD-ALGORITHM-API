8971dc7a4182f64ca0049eead002fb00a229d7c7
s.karekkattumanasree <s.karekkattumanasree@CA-L9Z2KNN2.groupinfra.com>
2020-12-16 09:39:20 -0800

    KeyCloak changes

--- api/src/main/java/ca/bc/gov/educ/api/gradalgorithm/util/APIUtils.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradalgorithm/util/APIUtils.java
@@ -1,17 +1,14 @@
 package ca.bc.gov.educ.api.gradalgorithm.util;
 
-import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.http.HttpHeaders;
 
 public class APIUtils {
 
-    public static HttpHeaders getHeaders (String username, String secret)
+	public static HttpHeaders getHeaders (String accessToken)
     {
-        String basicAuth = Base64.encodeBase64String((username + ":" + secret).getBytes());
-
-        HttpHeaders httpHeaders = new HttpHeaders();
+		HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add("Content-Type", "application/json");
-        httpHeaders.add("Authorization", "Basic " + basicAuth);
+        httpHeaders.setBearerAuth(accessToken);
         return httpHeaders;
     }
 
