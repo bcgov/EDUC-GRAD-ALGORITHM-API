58222e0d76395889acad838d946521ec00684f6b
alexryba <alex.rybakov@gov.bc.ca>
2021-06-18 14:43:01 -0700

    Enable maven tests

--- api/src/test/java/ca/bc/gov/educ/api/gradalgorithm/EducGradAlgorithmApiApplicationTests.java
+++ api/src/test/java/ca/bc/gov/educ/api/gradalgorithm/EducGradAlgorithmApiApplicationTests.java
@@ -1,11 +1,6 @@
 package ca.bc.gov.educ.api.gradalgorithm;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import ca.bc.gov.educ.api.gradalgorithm.dto.Student;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class EducGradAlgorithmApiApplicationTests {
