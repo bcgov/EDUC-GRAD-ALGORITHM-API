8d3d177ede9bfc10cde05376bdf8cc1b1920e3dc
s.karekkattumanasree <s.karekkattumanasreekumaran@cgi.com>
2022-02-25 14:48:01 -0800

    GRAD2-988

--- api/src/main/java/ca/bc/gov/educ/api/gradalgorithm/service/GradAlgorithmService.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradalgorithm/service/GradAlgorithmService.java
@@ -228,7 +228,7 @@ public class GradAlgorithmService {
 	private void processMessageForUnGraduatedStudent(GradMessageRequest gradMessageRequest, StringBuilder strBuilder, TranscriptMessage result, Map<String, OptionalProgramRuleProcessor> mapOptional,RuleProcessorData ruleProcessorData) {
 		getMessageForProjected(gradMessageRequest,strBuilder,result);
 		if(!gradMessageRequest.getGradProgram().equalsIgnoreCase(SCCP)) {
-			createCompleteGradMessage(strBuilder,result,mapOptional,ruleProcessorData);
+			createCompleteGradMessage(strBuilder,result,mapOptional,ruleProcessorData,"fromNonGrad");
 		}
 	}
 	private void processMessageForGraduatedStudent(GradMessageRequest gradMessageRequest, StringBuilder strBuilder, TranscriptMessage result, Map<String, OptionalProgramRuleProcessor> mapOptional,RuleProcessorData ruleProcessorData) {
@@ -240,7 +240,7 @@ public class GradAlgorithmService {
 			}
 			strBuilder.append(" ").append(String.format(result.getGradDateMessage(),formatGradDate(gradMessageRequest.getGradDate())));
 			strBuilder.append(". ");
-			createCompleteGradMessage(strBuilder,result,mapOptional,ruleProcessorData);
+			createCompleteGradMessage(strBuilder,result,mapOptional,ruleProcessorData,"fromGraduated");
 		}else {
 			getMessageForProjected(gradMessageRequest,strBuilder,result);
 		}
@@ -488,7 +488,7 @@ public class GradAlgorithmService {
 		}
 	}
 
-	private void createCompleteGradMessage(StringBuilder currentGradMessage, TranscriptMessage result, Map<String,OptionalProgramRuleProcessor> mapOptional, RuleProcessorData ruleProcessorData) {
+	private void createCompleteGradMessage(StringBuilder currentGradMessage, TranscriptMessage result, Map<String,OptionalProgramRuleProcessor> mapOptional, RuleProcessorData ruleProcessorData,String opMessage) {
 
 		List<String> programs = new ArrayList<>();
 		List<String> optPrograms = new ArrayList<>();
@@ -497,12 +497,15 @@ public class GradAlgorithmService {
 		for (Map.Entry<String, OptionalProgramRuleProcessor> entry : mapOptional.entrySet()) {
 			String optionalProgramCode = entry.getKey();
 			OptionalProgramRuleProcessor obj = entry.getValue();
-			if(optionalProgramCode.compareTo("AD")==0 || optionalProgramCode.compareTo("BD")==0 || optionalProgramCode.compareTo("BC")==0) {
-				programs.add(obj.getOptionalProgramName());
-			}else if(optionalProgramCode.compareTo("CP")==0) {
-				cpCommaSeparated = getCareerProgramNames(ruleProcessorData);
-			}else {
-				optPrograms.add(obj.getOptionalProgramName());
+
+			if(opMessage.equalsIgnoreCase("fromNonGrad") || (obj.isOptionalProgramGraduated() && opMessage.equalsIgnoreCase("fromGraduated"))) {
+				if (optionalProgramCode.compareTo("AD") == 0 || optionalProgramCode.compareTo("BD") == 0 || optionalProgramCode.compareTo("BC") == 0) {
+					programs.add(obj.getOptionalProgramName());
+				} else if (optionalProgramCode.compareTo("CP") == 0) {
+					cpCommaSeparated = getCareerProgramNames(ruleProcessorData);
+				} else {
+					optPrograms.add(obj.getOptionalProgramName());
+				}
 			}
 		}
 		if(StringUtils.isNotBlank(cpCommaSeparated)) {
