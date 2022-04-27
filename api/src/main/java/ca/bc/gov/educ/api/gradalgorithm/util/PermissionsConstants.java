ca93203aa6fa32f28f21bc35f4d7365dcf2f4895
Kamal <kamal.mohammed@outlook.com>
2022-04-27 17:06:37 -0600

    OAuth updates

--- api/src/main/java/ca/bc/gov/educ/api/gradalgorithm/util/PermissionsConstants.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradalgorithm/util/PermissionsConstants.java
@@ -5,5 +5,5 @@ public class PermissionsConstants {
 	public static final String _PREFIX = "hasAuthority('";
 	public static final String _SUFFIX = "')";
 
-	public static final String RUN_GRAD_ALGORITHM = _PREFIX + "RUN_GRAD_ALGORITHM" + _SUFFIX;
+	public static final String RUN_GRAD_ALGORITHM = _PREFIX + "SCOPE_RUN_GRAD_ALGORITHM" + _SUFFIX;
 }
