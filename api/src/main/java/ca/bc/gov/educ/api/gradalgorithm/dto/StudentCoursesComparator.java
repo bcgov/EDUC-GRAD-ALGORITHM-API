a24c1258584bcb9fdc57747796c221c3e0c3c6b3
s.karekkattumanasree <s.karekkattumanasreekumaran@cgi.com>
2022-05-09 16:06:24 -0700

    GRAD2-1197

---
+++ api/src/main/java/ca/bc/gov/educ/api/gradalgorithm/dto/StudentCoursesComparator.java
@@ -0,0 +1,38 @@
+package ca.bc.gov.educ.api.gradalgorithm.dto;
+import java.util.Comparator;
+
+import org.apache.commons.lang3.builder.CompareToBuilder;
+
+public class StudentCoursesComparator implements Comparator<StudentCourse> {
+
+    private String program;
+    public StudentCoursesComparator(String program) {
+        this.program = program;
+    }
+
+    @Override
+    public int compare(StudentCourse o1, StudentCourse o2) {
+        int res = 0;
+        switch (program) {
+            case "2018-EN":
+            case "2004-EN":
+            case "2004-PF":
+                res= new CompareToBuilder()
+                        .append(o2.getCompletedCoursePercentage(), o1.getCompletedCoursePercentage())
+                        .append(o2.getCredits(), o1.getCredits())
+                        .append(o2.getCourseLevel(), o1.getCourseLevel())
+                        .append(o2.getSessionDate(), o1.getSessionDate()).toComparison();
+                break;
+            case "2018-PF":
+                res= new CompareToBuilder()
+                        .append(o2.getCompletedCoursePercentage(), o1.getCompletedCoursePercentage())
+                        .append(o2.getCredits(), o1.getCredits())
+                        .append(o2.getCourseLevel(), o1.getCourseLevel()).toComparison();
+                break;
+            default:
+
+        }
+        return res;
+
+    }
+}
