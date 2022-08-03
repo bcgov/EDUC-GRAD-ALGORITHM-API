db4d0a6f968b0d411178cbecde8f6906f3de3fbe
alexryba <alex.rybakov@gov.bc.ca>
2022-08-03 13:46:06 -0600

    [GRAD2-1409] Update security implementation of all APIs migrated from springboot 2.4 to 2.7

---
+++ api/src/main/java/ca/bc/gov/educ/api/gradalgorithm/config/GradAlgorithmApiConfig.java
@@ -0,0 +1,22 @@
+package ca.bc.gov.educ.api.gradalgorithm.config;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class GradAlgorithmApiConfig {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
