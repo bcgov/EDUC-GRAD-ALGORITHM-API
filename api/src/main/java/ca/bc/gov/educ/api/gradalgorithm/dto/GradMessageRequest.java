a42c393e03a80a4ad60962d478e39c25da955cd1
alexryba <alex.rybakov@gov.bc.ca>
2022-12-01 13:00:14 -0700

    GRADT-465 2004 Program TVR Grad Message incorrect

--- api/src/main/java/ca/bc/gov/educ/api/gradalgorithm/dto/GradMessageRequest.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradalgorithm/dto/GradMessageRequest.java
@@ -2,6 +2,7 @@ package ca.bc.gov.educ.api.gradalgorithm.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 
 @Data
 @Builder
@@ -13,4 +14,8 @@ public class GradMessageRequest {
     String programName;
     String schoolAtGradName;
     boolean projected;
+
+    public boolean isPullGraduatedMessage() {
+        return StringUtils.isNotBlank(gradDate);
+    }
 }
