891cc1a5182707042cb3eee4d5916db04d98e021
Jinil Sung <infstar@gmail.com>
2023-06-07 16:46:33 -0700

    GRAD2-2135: TVR job crash is fixed.
    GRAD2-2135: TVR job crash is fixed.

--- api/src/main/java/ca/bc/gov/educ/api/gradalgorithm/util/GradAlgorithmAPIConstants.java
+++ api/src/main/java/ca/bc/gov/educ/api/gradalgorithm/util/GradAlgorithmAPIConstants.java
@@ -29,7 +29,7 @@ public class GradAlgorithmAPIConstants {
     protected static final Date DEFAULT_UPDATED_TIMESTAMP = new Date();
 
     //Default Date format constants
-    public static final String DEFAULT_DATE_FORMAT = "dd-MMM-yyyy";
+    public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
     public static final String SECONDARY_DATE_FORMAT = "yyyy/MM/dd";
 
     //Rule Type Constants
