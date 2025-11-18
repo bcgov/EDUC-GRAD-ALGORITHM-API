4fb6bf68fce2dae17541dc258fd432588a7020a5
mightycox <j.acox@hotmail.com>
2025-11-18 09:42:35 -0800

    Adds UT

--- api/src/test/java/ca/bc/gov/educ/api/gradalgorithm/util/APIUtilsTests.java
+++ api/src/test/java/ca/bc/gov/educ/api/gradalgorithm/util/APIUtilsTests.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.http.HttpHeaders;
 
-import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class APIUtilsTests {
@@ -15,5 +13,27 @@ public class APIUtilsTests {
         HttpHeaders headers;
         headers = APIUtils.getHeaders("my-access-token");
         assertEquals("application/json", headers.get("Content-Type").get(0));
+    }
+
+    @Test
+    void getNumericCourseLevel_WithSingleDigitAndLetter_ShouldReturnNumeric() {
+        assertEquals(1, APIUtils.getNumericCourseLevel("1A"));
+    }
+
+    @Test
+    void getNumericCourseLevel_WithDoubleDigitAndLetter_ShouldReturnNumeric() {
+        assertEquals(12, APIUtils.getNumericCourseLevel("12A"));
+    }
+
+    @Test
+    void getNumericCourseLevel_WithNumericOnly_ShouldReturnNumeric() {
+        assertEquals(123, APIUtils.getNumericCourseLevel("123"));
+    }
+
+    @Test
+    void getNumericCourseLevel_WithNullOrEmpty_ShouldReturnZero() {
+        assertEquals(0, APIUtils.getNumericCourseLevel(null));
+        assertEquals(0, APIUtils.getNumericCourseLevel(""));
+        assertEquals(0, APIUtils.getNumericCourseLevel("A"));
     }
 }
